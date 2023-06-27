fff50f1f804ff4fdcf6576377c7a1bb272cf41ce
pdkkg <kushwahagautam24@gmail.com>
2023-06-27 17:56:44 +0530

    portfolio Updated

--- pages/Components/Layout.js
+++ pages/Components/Layout.js
@@ -2,10 +2,7 @@ import React from 'react'
 
 import Navbar from './Navbar'
 import Footer from './Footer'
-import About from '../Gautam/About';
-import Cards from '../Gautam/Cards';
-import Experience from '../Gautam/Experience';
-import Projects from '../Gautam/Projects';
+
 
 export default function Layout({ children }) {
   return (
